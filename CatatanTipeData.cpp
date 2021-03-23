3a9c2cb484fa6271f33f4f930298800f1bcecd1b
Muhammad Nur Alamsyah <67987788+Nur47-blip@users.noreply.github.com>
2021-03-23 14:45:01 +0700

    Create CatatanTipeData.cpp

---
+++ CatatanTipeData.cpp
@@ -0,0 +1,30 @@
+#include <iostream> 
+
+using namespace std;
+
+int main(){
+	
+//	bilangan bulat
+//	int
+//	
+//	huruf
+//	char
+//	
+//	kata
+//	string
+//	
+//	bilangan real (bisa pecahan)
+//	double
+//	
+//	boolean (benar salah)
+//	bool
+	
+//	ini namanya comment, tidak ikut tercompile
+	
+	int bilangan_bulat; //nilainya masih random
+	int Bilangan_bulat = 5; //nilainya langsung 5
+	
+	Bilangan_bulat = 6; //mengganti nilai Bilangan_bulat jadi 6
+	cout << Bilangan_bulat;
+	
+}
